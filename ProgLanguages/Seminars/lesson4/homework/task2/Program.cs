d353c228351fe7c8bf0b785632afa4f5d19aa157
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-22 21:28:11 +0300

    add task2/homework/lesson4/seminars

---
+++ ProgLanguages/Seminars/lesson4/homework/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+void EvenParity(int[] arr)
+{
+    Console.Write("чётные:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("массив:");
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+Console.WriteLine("size:");
+int n = Int32.Parse(Console.ReadLine());
+int[] arr = CreateArray(n);
+EvenParity(arr);
+PrintArray(arr);
