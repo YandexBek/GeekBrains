c627c365e5979432b35a42fca82b83589a970d75
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-23 22:24:28 +0300

    modified task3:homework

--- ProgLanguages/Seminars/lesson4/homework/task1/Program.cs
+++ ProgLanguages/Seminars/lesson4/homework/task1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Write("Для выхода введите (q) или число: ");
+    string str = Console.ReadLine();
+    if (str == "q")
+    {
+        Console.Write("стоп");
+        break;
+    }
+    int n = Int32.Parse(str);
+    int number = SumElements(n);
+    if(n % 2 == 0)
+    {
+        Console.Write("стоп");
+        break;
+    }
+    else
+    {
+        Console.Write("сумма цифр в числе должно быть четным! ");
+    }
+
+}
+
+int SumElements(int num)
+{
+    int i = 1;
+    int sum = 0;
+    while(i <= num)
+    {
+        sum = sum + num % 10;
+        i++;
+    }
+    return sum;
+}
--- ProgLanguages/Seminars/lesson4/homework/task3/Program.cs
+++ ProgLanguages/Seminars/lesson4/homework/task3/Program.cs
@@ -1,2 +1,40 @@
-﻿Console.WriteLine("Введите размер массива:");
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+}
+
+void ReversArray(int[] arr)
+{
+     int firstNumber = 0;
+     int lastNumber = arr.Length - 1;
+     while(firstNumber < lastNumber)
+     {
+        int mind = arr[firstNumber];
+        arr[firstNumber] = arr[lastNumber];
+        arr[lastNumber] = mind;
+        firstNumber++;
+        lastNumber--;
+     }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e}, ");
+    }
+    Console.Write(";");
+}
+
+Console.WriteLine("size:");
 int n = Int32.Parse(Console.ReadLine());
+int[] arr = CreateArray(n);
+PrintArray(arr);
+ReversArray(arr);
+PrintArray(arr);
