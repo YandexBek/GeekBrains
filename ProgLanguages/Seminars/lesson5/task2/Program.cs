871d085b48ec5e663158aee2ac48964bd84becd3
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-28 20:46:03 +0300

    add task2 in lesson5: Seminars

---
+++ ProgLanguages/Seminars/lesson5/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿int[,] matrix =
+{
+    {1 ,2, 3, 9},
+    {4, 5, 6, 10},
+    {7, 8, 9, 8}
+};
+
+int GetSumOnDiag(int[,] table)
+{
+    int sum = 0;
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += table[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintTable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintTable(matrix);
+int[,] table = GetSumOnDiag(matrix);
+PrintTable(table);
