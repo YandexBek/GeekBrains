a44955c5f84d1348b3f5ace7b2ed92871543a554
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-28 20:18:29 +0300

    add task1 in lesson5: Seminars

---
+++ ProgLanguages/Seminars/lesson5/task1/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,] matrix =
+{
+    {1 ,2, 3, 9},
+    {4, 5, 6, 10},
+    {7, 8, 9, 8}
+};
+
+void ReplaseElemsBySquares(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                table[i, j] = table[i, j] * table[i, j];
+            }
+        }
+    }
+}
+
+void PrintTable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintTable(matrix);
+ReplaseElemsBySquares(matrix);
+PrintTable(matrix);
