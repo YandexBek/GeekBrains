3218cebe346f8f04b9604a6aa2487a5c29c47edf
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-28 10:56:26 +0300

    modifTask1, add task2 in lesson4: lectures

--- ProgLanguages/Lectures/lesson4/task1/Program.cs
+++ ProgLanguages/Lectures/lesson4/task1/Program.cs
@@ -1,19 +1,28 @@
-﻿int[,] matrix = new int[3, 5];
-
-Random rnd = new Random();
-for (int i = 0; i < matrix.GetLength(0); i++)
+﻿int[,] CreateMatrix( int rowCount, int columsCount)
 {
-    for (int j = 0; matrix.GetLength(1); j++)
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
         matrix[i, j] = rnd.Next(1, 11);
+        }
     }
+    return matrix;
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+void PrintMatrix(int[,] matrix)
 {
-    for (int j = 0; matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write($"{matrix[i, j]} ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
+int[,] matrix = CreateMatrix(4,5);
+PrintMatrix(matrix);
