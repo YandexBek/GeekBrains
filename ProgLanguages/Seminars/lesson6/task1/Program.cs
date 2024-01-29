9b6d4d1df774f4189a43748e98eb4803777ae717
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-29 22:17:52 +0300

    add tasf1 int lesson6: seminars

---
+++ ProgLanguages/Seminars/lesson6/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumOfDigits(int num)
+{
+    if(num == 0)
+    {
+        return 0;
+    }
+    int sum = num % 10 + SumOfDigits(num / 10);
+    return sum;
+}
+
+int res = SumOfDigits(345);
+Console.WriteLine(res);
