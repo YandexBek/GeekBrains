cf78ee9fd9b54fc7ef04cacc1eff8a18143bb5c4
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-31 21:38:11 +0300

    add task1 in homework:lesson7

---
+++ ProgLanguages/Seminars/lesson7/homework/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿void GetNumInSection(int minNum, int maxNum)
+{
+    if (minNum <= maxNum)
+    {
+        Console.Write($"{minNum} ");
+        GetNumInSection(minNum + 1, maxNum);
+    }
+}
+
+Console.WriteLine("Введите значение M:");
+int M = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N:");
+int N = Int32.Parse(Console.ReadLine());
+GetNumInSection(M, N);
