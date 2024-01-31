fd2d2cab2d2eab7e5eaca199b219065faf4cffc5
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-31 16:53:17 +0300

    add task1 in lesson5: lectures

---
+++ ProgLanguages/Lectures/lesson5/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вычислить факториал от натурального числа N
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine(Fact(5));
