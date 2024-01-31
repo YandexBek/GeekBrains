dd6e498a36cdf5566fcf54009461fc2003fbeda0
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-01-31 22:16:07 +0300

    add task1 in homework:lesson6

---
+++ ProgLanguages/Seminars/lesson6/homework/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿char[,] charArray = new char[,]
+        {
+            { 'H', 'e', 'l', 'l' },
+            { 'o', ' ', 'W', 'o' },
+            { 'r', 'l', 'd', '!' }
+        };
+
+int rowCount = charArray.GetLength(0);
+int colCount = charArray.GetLength(1);
+
+string result = "";
+for (int i = 0; i < rowCount; i++)
+{
+    for (int j = 0; j < colCount; j++)
+    {
+        result += charArray[i, j];
+    }
+}
+
+Console.WriteLine(result);
