fb3cba00da38405a7991321ac71bbdfd6d3ce6a1
Ulugbek Khodzhayev <ukhodzhayev@inbox.ru>
2024-02-04 15:50:45 +0300

    add task2 in homework:lesson7

--- ProgLanguages/Seminars/lesson7/homework/task2/Program.cs
+++ ProgLanguages/Seminars/lesson7/homework/task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
+int m = 2;
+int n = 1;
+int result = AckermannFunction(m, n);
+Console.WriteLine(result);
